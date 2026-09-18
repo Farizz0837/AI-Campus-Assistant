24c0bd676dcdc1f9e32d1c3c7d970be136b9aeaa
AniqFarsis <aniqfarisshahadam@gmail.com>
2026-09-18 22:30:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function sendMessage() {
+    const input = document.getElementById("userInput");
+    const chat = document.getElementById("chat");
 
+    const message = input.value.trim();
+
+    if (message === "") return;
+
+    chat.innerHTML += `<p><b>You:</b> ${message}</p>`;
+
+    chat.innerHTML += `
+        <p class="bot">
+            Thanks for your question! AI Campus Assistant
+            is being developed.
+        </p>
+    `;
+
+    input.value = "";
+}
